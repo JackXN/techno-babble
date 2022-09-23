a74d77ff084872392dcd53e94cf163f0d7a549f5
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-09-22 21:33:03 -0600

    chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,31 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import React, {useState, useEffect} from 'react'
 import { Layout } from '../components';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "blue",
+    },
+    primary: {
+    'orange': '#F47F39',
+      'gray': '#787878',
+      'white': '#fffff'
+    }
+  },
+})
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <ChakraProvider theme={theme}>
       <Component {...pageProps}/>
+      </ChakraProvider>
     </Layout>
   )
 }
